bbe1c38ad2b3a638d65f6758bc93210c25591585
Daniel <hikingwithswift@gmail.com>
2021-04-10 18:00:02 -0600

    Initial Commit

---
+++ OneTimeCodeBoxes/OneTimeCodeBoxesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneTimeCodeBoxesApp.swift
+//  OneTimeCodeBoxes
+//
+//  Created by HikingWithSwift on 2021-04-10.
+//
+
+import SwiftUI
+
+@main
+struct OneTimeCodeBoxesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
