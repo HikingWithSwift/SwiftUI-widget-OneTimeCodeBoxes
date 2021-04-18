2302baba432bc7bc72dbe9556146822773a84ccd
HikingWithSwift <hikingwithswift@gmail.com>
2021-04-17 21:17:26 -0600

    Create folder and two files

---
+++ OneTimeCodeBoxes/OneTimeCode/OneTimeCodeBoxes.swift
@@ -0,0 +1,20 @@
+//
+//  OneTimeCodeBoxes.swift
+//  OneTimeCodeBoxes
+//
+//  Created by HikingWithSwift on 2021-04-17.
+//
+
+import SwiftUI
+
+struct OneTimeCodeBoxes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OneTimeCodeBoxes_Previews: PreviewProvider {
+    static var previews: some View {
+        OneTimeCodeBoxes()
+    }
+}
---
+++ OneTimeCodeBoxes/OneTimeCode/OneTimeCodeInput.swift
@@ -0,0 +1,12 @@
+//
+//  OneTimeCodeInput.swift
+//  OneTimeCodeBoxes
+//
+//  Created by HikingWithSwift on 2021-04-17.
+//
+
+import SwiftUI
+
+struct OneTimeCodeInput: UIViewRepresentable {
+    
+}
